b14d6f5d861e8d40c02296bf29e8c8a729f45413
Rai Dorzback <144272001+raisa-d@users.noreply.github.com>
2025-02-06 13:19:16 -0500

    Update sumOfVariableLength.js

--- sumOfVariableLength.js
+++ sumOfVariableLength.js
@@ -1,21 +1,14 @@
 /*
 https://leetcode.com/problems/sum-of-variable-length-subarrays/
 You are given an integer array nums of size n. For each index i where 0 <= i < n, define a 
-subarray
- nums[start ... i] where start = max(0, i - nums[i]).
+subarray nums[start ... i] where start = max(0, i - nums[i]).
 
 Return the total sum of all elements from the subarray defined for each index in the array.
-
- 
-
 Example 1:
-
 Input: nums = [2,3,1]
-
 Output: 11
 
 Explanation:
-
 i	Subarray	Sum
 0	nums[0] = [2]	2
 1	nums[0 ... 1] = [2, 3]	5
@@ -24,13 +17,10 @@ Total Sum	 	11
 The total sum is 11. Hence, 11 is the output.
 
 Example 2:
-
 Input: nums = [3,1,1,2]
-
 Output: 13
 
 Explanation:
-
 i	Subarray	Sum
 0	nums[0] = [3]	3
 1	nums[0 ... 1] = [3, 1]	4
@@ -39,10 +29,7 @@ i	Subarray	Sum
 Total Sum	 	13
 The total sum is 13. Hence, 13 is the output.
 
- 
-
 Constraints:
-
 1 <= n == nums.length <= 100
 1 <= nums[i] <= 1000
 */
@@ -51,7 +38,7 @@ Constraints:
 // P: arr of nums, R: sum
 function subarraySum(nums) {
     let subarrays = [];
-    // loop through nums - i
+    // loop through nums 
     for(let i = 0; i < nums.length; i++) {
         let subarr = [];
         // calculate start, subarr: nums[start] thru nums[i]
@@ -67,36 +54,16 @@ function subarraySum(nums) {
     return sum;
 };
 
-console.log(subarraySum([2, 3, 1]), 11);
-
-// in progress answer
-// nums arr
-/*
-const subarraySum = nums => {
-    let sum = [];
-    // loop through array
-    const n = nums.length;
-    for(let i = 0; i < n; i++) {
-        if(i === 0) {
-            sum.push(nums[i])
-        } else if(i >= 0 && i < n) {
-            const start = Math.max(0, i - nums[i]);
-            // create subarray by slicing from nums[start] through nums[i]
-            let subarray = nums.slice(nums[start], nums[i+1]);
-            console.log(`starting index: ${start}`)
-            console.log(`Subarray ${i+1} start: ${nums[start]}\nEnd: ${nums[i]}`);
-            console.log(`Subarray: ${subarray}`)
-            // add suba rray to sum
-            sum.push(subarray);
-        } else {
-            sum.push(nums[i]);
-        };
+// SOLUTION 2: use slice instead of a loop in a loop. also O(n^2)
+function subarraySum(nums) {
+    let subarrays = [];
+    for(let i = 0; i < nums.length; i++) {
+        const start = Math.max(0, i - nums[i]);
+        let subarr = nums.slice(start, i+1)
+        console.log(`Subarray ${i+1}: ${subarr}`)
+        subarrays.push(subarr);
     };
-    return sum
-    // add up numbers in sum
-    // sum = sum.flat().reduce((a, b) => a + b, 0);
-    // return sum;
+    const sum = subarrays.flat().reduce((a, b) => a + b, 0);
+    return sum;
 };
-
-console.log(subarraySum([2, 3, 1]), 11)
-*/
+console.log(subarraySum([2, 3, 1]), 11);
